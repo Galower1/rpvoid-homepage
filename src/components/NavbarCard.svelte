367c04cc34200f3ae4d63a69a8296f84bc48b3ab
Galower <ariosgalower@gmail.com>
2021-12-17 23:05:36 -0400

    navbar card animations

--- src/components/NavbarCard.svelte
+++ src/components/NavbarCard.svelte
@@ -1,15 +1,30 @@
 <script>
+	import { page } from '$app/stores';
+
 	export let image = '';
 	export let linkURL = '';
+
+	let matchURL = false;
+	$: matchURL = $page.path !== linkURL;
 </script>
 
 <a sveltekit:prefetch href={linkURL}>
-	<img src={image} alt={linkURL} />
+	<img src={image} alt={linkURL} class:active={!matchURL} />
 </a>
 
 <style>
 	img {
+		position: relative;
 		width: 160px;
 		height: 130px;
+		bottom: 20px;
+		transition: 0.7s;
+		filter: brightness(0.5) grayscale(0.4);
+	}
+
+	.active {
+		transition: 0.7s;
+		bottom: 0px;
+		filter: none;
 	}
 </style>
