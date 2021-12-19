8223c665356498eee701cefdd2c1f4519e52d60b
Galower <ariosgalower@gmail.com>
2021-12-19 16:10:08 -0400

    navbar images and backgrounds now use webp format

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <nav>
-	<NavbarCard image={'/media/navbar/home.png'} linkURL={'/'} />
-	<NavbarCard image={'/media/navbar/login.png'} linkURL={'/account'} />
-	<NavbarCard image={'/media/navbar/key.png'} linkURL={'/key'} />
-	<NavbarCard image={'/media/navbar/patreon.png'} linkURL={'/patreon'} />
-	<NavbarCard image={'/media/navbar/discord.png'} linkURL={'/discord'} />
+	<NavbarCard image={'/media/navbar/home.webp'} linkURL={'/'} />
+	<NavbarCard image={'/media/navbar/login.webp'} linkURL={'/account'} />
+	<NavbarCard image={'/media/navbar/key.webp'} linkURL={'/key'} />
+	<NavbarCard image={'/media/navbar/patreon.webp'} linkURL={'/patreon'} />
+	<NavbarCard image={'/media/navbar/discord.webp'} linkURL={'/discord'} />
 </nav>
 
 <style>
--- src/components/Parallax.svelte
+++ src/components/Parallax.svelte
@@ -1,10 +1,11 @@
-<div class="parallax" />
+<div class="parallax-container"><div class="parallax" /></div>
 
 <style>
 	.parallax {
-		height: 200px;
-		background-image: url('/media/backgrounds/1.png'), url('/media/backgrounds/2.png'),
-			url('/media/backgrounds/3.png'), url('/media/backgrounds/4.png');
+		height: 250px;
+		background-size: 1700px;
+		background-image: url('/media/backgrounds/1.webp'), url('/media/backgrounds/2.webp'),
+			url('/media/backgrounds/3.webp'), url('/media/backgrounds/4.webp');
 		background-repeat: repeat-x, repeat-x, repeat-x, repeat-x;
 		-webkit-animation: animate-background 50s linear infinite;
 		animation: animate-background 50s linear infinite;
@@ -16,7 +17,7 @@
 		}
 
 		100% {
-			background-position: 2000px, 100%, 1000px 100%, 500px 100%, 250px 100%;
+			background-position: 2000px 100%, 1000px 100%, 500px 100%, 250px 100%;
 		}
 	}
 </style>
